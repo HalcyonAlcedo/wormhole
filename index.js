10bb37465006aef282630cef29310139495f99b1
zyc404 <zyc404@gmail.com>
2024-05-11 14:36:55 +0800

    允许全部访问

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ fastify.get('/', (request, reply) => {
 })
 const start = async () => {
     try {
-        await fastify.listen({ port: 3000 })
+        await fastify.listen({ port: 3000, host: '::'})
         console.log('wormhole服务已启动')
     } catch (err) {
         fastify.log.error(err)
