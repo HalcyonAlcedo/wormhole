0e4d3a9f2971eef8fdd4c70eeea5efcf8ad9bfcc
zyc404 <zyc404@gmail.com>
2024-05-16 12:00:29 +0800

    添加对post请求api的支持

--- src/routes/web.js
+++ src/routes/web.js
@@ -3,7 +3,7 @@
 import { parse } from 'path'
 
 export default async function webRoutes(fastify, options) {
-  
+
   const { clientManager, webDataStore } = options.options
 
   // 处理客户端Web请求
@@ -16,7 +16,29 @@ export default async function webRoutes(fastify, options) {
 
     try {
       // 创建一个新的Promise来等待WebSocket客户端的响应
-      const data = await waitForWsResponse(clientManager, webDataStore, clientId, path, queryParams)
+      const data = await getWaitForWsResponse(clientManager, webDataStore, clientId, path, queryParams)
+      if (data.code === 301 || data.code === 302) {
+        reply.redirect(data.code, data.target)
+      } else {
+        reply.code(data.code).type(data.type).send(data.data)
+      }
+    } catch (error) {
+      reply.code(500).send({ error: error.message })
+    }
+  })
+
+  // 处理客户端api请求
+  await fastify.post('/:clientId/*', async (request, reply) => {
+    const clientId = parseInt(request.params.clientId)
+    const path = request.params['*']
+    const queryParams = request.query
+    const bodyParams = request.body
+
+    console.log(`客户端${clientId}请求数据:`, path)
+
+    try {
+      // 创建一个新的Promise来等待WebSocket客户端的响应
+      const data = await postWaitForWsResponse(clientManager, webDataStore, clientId, path, queryParams, bodyParams)
       if (data.code === 301 || data.code === 302) {
         reply.redirect(data.code, data.target)
       } else {
@@ -28,7 +50,7 @@ export default async function webRoutes(fastify, options) {
   })
 
   // 定义waitForWsResponse函数来处理WebSocket响应
-  async function waitForWsResponse(clientManager, webDataStore, clientId, path, queryParams) {
+  async function getWaitForWsResponse(clientManager, webDataStore, clientId, path, queryParams) {
     const pathObj = parse(path)
     path = `${pathObj.dir}${pathObj.dir ? '/' : ''}${pathObj.base}`
     return new Promise((resolve, reject) => {
@@ -121,6 +143,54 @@ export default async function webRoutes(fastify, options) {
         }, 60000) // 1分钟
       } else {
         resolve({ type: 'text/html', data: 'Client not found', code: 403 })
+      }
+    })
+  }
+  async function postWaitForWsResponse(clientManager, webDataStore, clientId, path, queryParams, bodyParams) {
+    const pathObj = parse(path)
+    path = `${pathObj.dir}${pathObj.dir ? '/' : ''}${pathObj.base}`
+    return new Promise((resolve, reject) => {
+      const client = clientManager.getClient(clientId)
+      if (client) {
+        webDataStore.initDataStore(clientId)
+        client.send(JSON.stringify({ type: 'web', path, query: queryParams, body: bodyParams, command: 'post' }))
+        // 设置事件监听器来处理WebSocket客户端返回的消息
+        const messageHandler = message => {
+          const ret = data => {
+            client.removeListener('message', messageHandler)
+            resolve(data)
+          }
+          try {
+            message = JSON.parse(message)
+          } catch (error) {
+            webDataStore.clearData(clientId, path)
+            ret({ type: 'text/html', data: `非法消息${message}`, code: 403 })
+          }
+          if (message.type === 'web' && message.path === path && message.data != undefined && message.command === 'postapi') {
+            if (message.state === 'complete') {
+              ret({ type: 'application/json', data: message.data, code: 200 })
+            } else if (message.state === 'error') {
+              webDataStore.clearData(clientId, path)
+              client.removeListener('message', messageHandler)
+              reject(new Error(`Get Data Error${error.message}`))
+            }
+          }
+          if (message.type === 'web' && message.path === path && message.command === 'redirect' && message.target) {
+            ret({ code: 301, target: `/web/${clientId}/${message.target}` })
+          }
+          if (message.state === 'error') {
+            webDataStore.clearData(clientId, path)
+            ret({ type: 'application/json', data: message.error.message || message.error, code: message.error.status || 500 })
+          }
+        }
+        client.on('message', messageHandler)
+        // 设置超时
+        setTimeout(() => {
+          client.removeListener('message', messageHandler)
+          reject(new Error('Get Data Time Out')) // 拒绝Promise并返回错误
+        }, 60000) // 1分钟
+      } else {
+        resolve({ type: 'application/json', data: '{"error": "Client not found"}', code: 403 })
       }
     })
   }
